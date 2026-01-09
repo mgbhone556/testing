497c7ae5d6809ead874c7834ef0e507e8a368ab6
mgbhone556 <mgbhone556@gmail.com>
2026-01-09 16:10:35 +0630

    Initial Flutter project

---
+++ lib/model.dart
@@ -0,0 +1,18 @@
+class User {
+  final String id;
+  final String name;
+  final String email;
+  final int age;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.age,
+  });
+
+  @override
+  String toString() {
+    return 'User(id: $id, name: $name, email: $email, age: $age)';
+  }
+}
